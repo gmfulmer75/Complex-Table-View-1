e51a2dd2d5f2d59cf8fa67001fed8112abe054d5
Gavin Fulmer <gmfulmer@bellsouth.net>
2015-09-04 14:02:44 -0500

    I have added the RMGSolidColorView class files.  The table view's row configuration will have a small custom view that will draw a random color. The custom view will be set to this class.

---
+++ Complex Table View 1/RMGSolidColorView.m
@@ -0,0 +1,19 @@
+//
+//  RMGSolidColorView.m
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 9/4/15.
+//  Copyright © 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import "RMGSolidColorView.h"
+
+@implementation RMGSolidColorView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
