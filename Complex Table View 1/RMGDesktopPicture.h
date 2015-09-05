6466519e2ec05f606582889f5ad873ecdd0ac166
Gavin Fulmer <gmfulmer@bellsouth.net>
2015-09-05 07:44:39 -0500

    I have added the RMGDesktopPicture class to the project.  This will hold all the data for a single desktop picture.

---
+++ Complex Table View 1/RMGDesktopPicture.h
@@ -0,0 +1,13 @@
+//
+//  RMGDesktopPicture.h
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 9/5/15.
+//  Copyright © 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMGDesktopPicture : NSObject
+
+@end
---
+++ Complex Table View 1/RMGDesktopPicture.m
@@ -0,0 +1,13 @@
+//
+//  RMGDesktopPicture.m
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 9/5/15.
+//  Copyright © 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import "RMGDesktopPicture.h"
+
+@implementation RMGDesktopPicture
+
+@end
