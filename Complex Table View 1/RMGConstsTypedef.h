46cc75077f5ae2848ff29e16bbad47827987a89c
Gavin Fulmer <gmfulmer@bellsouth.net>
2015-09-11 09:29:52 -0500

    I have added these files to handle user defaults, error constants, and various typedefs for the program.

---
+++ Complex Table View 1/RMGConstsTypedef.h
@@ -0,0 +1,10 @@
+//
+//  RMGConstsTypedef.h
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 9/11/15.
+//  Copyright © 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
