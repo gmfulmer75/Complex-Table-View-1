92aae07452eb265ff1fb5f1fa0694e4115aed79e
Gavin Fulmer <gmfulmer@bellsouth.net>
2015-09-12 09:06:42 -0500

    I have added the suite of files for the preferences panel.

---
+++ Complex Table View 1/RMGPrefsWinController.m
@@ -0,0 +1,23 @@
+//
+//  RMGPrefsWinController.m
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 9/12/15.
+//  Copyright © 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import "RMGPrefsWinController.h"
+
+@interface RMGPrefsWinController ()
+
+@end
+
+@implementation RMGPrefsWinController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
