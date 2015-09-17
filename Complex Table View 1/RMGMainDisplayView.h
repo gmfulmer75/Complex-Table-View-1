d9f52b46d3fc2ca0a05d7487bfdf588a236d373b
Gavin Fulmer <gmfulmer@bellsouth.net>
2015-09-17 09:48:57 -0500

    I have added these class files to implement drag and drop with a standard NSImageView instance.

---
+++ Complex Table View 1/RMGMainDisplayView.h
@@ -0,0 +1,13 @@
+//
+//  RMGMainDisplayView.h
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 9/17/15.
+//  Copyright (c) 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RMGMainDisplayView : NSImageView
+
+@end
---
+++ Complex Table View 1/RMGMainDisplayView.m
@@ -0,0 +1,19 @@
+//
+//  RMGMainDisplayView.m
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 9/17/15.
+//  Copyright (c) 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import "RMGMainDisplayView.h"
+
+@implementation RMGMainDisplayView
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];    
+    // Drawing code here.
+}
+
+@end
