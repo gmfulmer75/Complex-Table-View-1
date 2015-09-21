36153ff41008a4116b1c14db7802d5d63eca3200
Gavin Fulmer <gmfulmer@bellsouth.net>
2015-09-21 10:55:46 -0500

    I have added the RMGPictureView class files for use in the drag and drop feature.

---
+++ Complex Table View 1/RMGPictureView.m
@@ -0,0 +1,19 @@
+//
+//  RMGPictureView.m
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 9/21/15.
+//  Copyright © 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import "RMGPictureView.h"
+
+@implementation RMGPictureView
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];    
+    // Drawing code here.
+}
+
+@end
