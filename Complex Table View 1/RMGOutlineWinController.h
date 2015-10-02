8d617254e4e57f55dc5a3944984669c6e7e26331
Gavin Fulmer <gmfulmer@bellsouth.net>
2015-10-02 13:33:57 -0500

    I have added the NSOutlineView window controller (with XIB) to implement drag and drop for the outline view.

---
+++ Complex Table View 1/RMGOutlineWinController.h
@@ -0,0 +1,13 @@
+//
+//  RMGOutlineWinController.h
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 10/2/15.
+//  Copyright © 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RMGOutlineWinController : NSWindowController
+
+@end
---
+++ Complex Table View 1/RMGOutlineWinController.m
@@ -0,0 +1,23 @@
+//
+//  RMGOutlineWinController.m
+//  Complex Table View 1
+//
+//  Created by Gavin Fulmer on 10/2/15.
+//  Copyright © 2015 RavenWorks Media Group, L.L.C. All rights reserved.
+//
+
+#import "RMGOutlineWinController.h"
+
+@interface RMGOutlineWinController ()
+
+@end
+
+@implementation RMGOutlineWinController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
